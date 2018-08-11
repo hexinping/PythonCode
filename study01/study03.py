722910922864892f7f26655a602fc83e19244f24
hexinping <hexinping456@126.com>
2018-08-11 18:14:40 +0800

    2

--- study01/study03.py
+++ study01/study03.py
@@ -21,7 +21,7 @@ def print_format():
     print "He's %d inches tall." % height
     print "He's got %s eyes and %s hair." % (eyes, hair)
     print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
-
+    print "I also said: '%s'." % age
 
 # 英寸转化成厘米
 def in_to_cm(i):
@@ -42,8 +42,27 @@ def inln_to_cmkg(in_value, lb_value):
     print "%d in to %f cm" % (in_value, cm_value), "%d lb to %f kg" % (lb_value, kg_value)
 
 
+def print_plus_format(w, s):
+    #两个字符串连接起来，组建成一个新的字符串
+    print w + s
+
+
+def print_in_oneline():
+
+   # print 后加,就可以一行输出
+
+   print "1",
+   print "2"
+
+
 if __name__ == "__main__":
 
     print_format()
     inln_to_cmkg(1, 1)
+
+    print_plus_format("2222", "/444")
+
+    print_in_oneline()
+
+
 
