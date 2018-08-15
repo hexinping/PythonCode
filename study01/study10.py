2ec8fd43b042e1576ecff4cf128ec2509c01b329
hexinping <hexinping456@126.com>
2018-08-15 22:29:52 +0800

    python restudy10

---
+++ study01/study10.py
@@ -0,0 +1,24 @@
+# #_*_ coding:utf-8 _*_
+
+
+
+def logic_fuhao():
+
+    print 'python 逻辑术语'
+
+    '''
+        • and与
+        • or或
+        • not非
+        • != (not equal) 不等于 
+        • ==(equal)等于
+        • >=(greater-than-equal)大于等于 
+        • <=(less-than-equal)小于等于
+        • True真
+        • False假
+    '''
+
+
+if __name__ == "__main__":
+
+    logic_fuhao()
