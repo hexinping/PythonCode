5a22c3931957a827165f76437aeafe2b60e0b2f0
hexinping <hexinping456@126.com>
2018-08-24 18:55:13 +0800

    2

---
+++ practice/py_datetime.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+
+import  datetime
+
+
+def datetime_func():
+
+    print datetime.datetime
+    print datetime.date
+    print datetime.time
+    print datetime.timedelta
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print "datetime----------"
+    datetime_func()
