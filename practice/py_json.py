30c1878613b591de6ae8ea117a14ca27653690ce
hexinping <hexinping456@126.com>
2018-08-30 18:43:23 +0800

    py json

--- practice/py_json.py
+++ practice/py_json.py
@@ -43,16 +43,19 @@ def py_to_json():
     # dump: 将数据写入json文件中
     with open("record.json", "w") as f:
         json.dump(test_dict, f)
+        f.close()
         print("加载入文件完成...")
 
     with open("record.json", 'r') as load_f:
         load_dict = json.load(load_f)
+        load_f.close()
         print(load_dict)
     load_dict['smallberg'] = [8200, {1: [['Python', 81], ['shirt', 300]]}]
     print(load_dict)
 
     with open("record.json", "w") as dump_f:
         json.dump(load_dict, dump_f)
+        dump_f.close()
 
 
 '''
