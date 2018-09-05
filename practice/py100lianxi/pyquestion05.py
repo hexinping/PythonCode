d6ccb4de22a9f163c427e68c5382a79c03f10cd3
hexinping <hexinping456@126.com>
2018-09-05 17:40:33 +0800

    py 100-05：输入三个整数x,y,z，请把这三个数由小到大输出。

--- practice/py100lianxi/pyquestion05.py
+++ practice/py100lianxi/pyquestion05.py
@@ -21,57 +21,23 @@ def sortedDictValues1(adict):
 
 def func1():
 
-    # a = int(raw_input(">"))
-    # b = int(raw_input(">"))
-    # c = int(raw_input(">"))
+    a = int(raw_input(">"))
+    b = int(raw_input(">"))
+    c = int(raw_input(">"))
 
-    # 使用默认的排序
-    # arr = []
-    # arr.append(a)
-    # arr.append(b)
-    # arr.append(c)
+    #使用默认的排序
+    arr = []
+    arr.append(a)
+    arr.append(b)
+    arr.append(c)
 
-    # arr1 = arr.sort() #不会产生新的列表 arr1为None,修改的是原有列表
-    #
-    # print arr
-    # print arr1  #输出为None
-    #
-    # arr2 = sorted([a,b,c]) #产生新的列表 arr2不为None
-    # print arr2
+    arr1 = arr.sort() #不会产生新的列表 arr1为None,修改的是原有列表
 
+    print arr
+    print arr1  #输出为None
 
-    #用自定义函数排序 使用sort的key关键字
-
-    # arr = []
-    # t1 = {"1":a}
-    # arr.append(t1)
-    # t2 = {"2": b}
-    # arr.append(t2)
-    # t3 = {"3": c}
-    # arr.append(t3)
-
-    # arr.sort(key = compare_ele)
-
-    # print arr
-
-
-    #这个是按照key值排序
-    test = {"2":1,"3":33,"4":2}
-    items = test.items()
-    items.sort()
-
-    test1 = [value for key, value in items]
-
-    print test1
-
-
-
-
-
-
-
-
-
+    arr2 = sorted([a,b,c]) #产生新的列表 arr2不为None
+    print arr2
 
 
 if __name__ == "__main__":
