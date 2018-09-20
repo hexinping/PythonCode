bf470b62390d460f4955677905501dc62653565d
hexinping <hexinping456@126.com>
2018-09-20 23:06:42 +0800

    py_net:BeautifulSoup 库

--- py_net/scrapetest.py
+++ py_net/scrapetest.py
@@ -9,9 +9,52 @@ urllib.parse 和 urllib.error。尽管函数名称大多和原来一样，但是
 的 urllib 库时需要注意哪些函数被移动到子模块里了
 '''
 
+'''
+<html>
+<head>
+<title>A Useful Page</title>
+</head>
+<body>
+<h1>An Interesting Title</h1>
+<div>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</div>
+</body>
+</html>
+
+
+'''
+
+from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(),features="html.parser")
+        title = bsObj.head.title
+        h1 = bsObj.body.h1
+
+        '''
+            这几种取法是一样的
+            bsObj.html.body.h1
+            bsObj.body.h1
+            bsObj.html.h1
+            bsObj.h1
+        '''
+    except AttributeError as e:
+        return None
+    return title,h1
+
 if __name__ == "__main__":
     print("1------")
 
-
-    html = urlopen("http://pythonscraping.com/pages/page1.html")
-    print(html.read())
+    title, h1 = getTitle("http://www.pythonscraping.com/pages/page1.html")
+    if title == None:
+        print("Title could not be found")
+    else:
+        print(title)
+        print(h1)
